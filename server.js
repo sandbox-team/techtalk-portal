dfb2518611fb807d4fca8df4a981ed8c6a921f78
putskoo <putskoo@gmail.com>
2013-09-01 13:06:13 +0300

    temp

--- server.js
+++ server.js
@@ -1,24 +1,32 @@
-require("colors");
-express = require("express");
+require('colors');
 
-var app = express();
-
-app
-  .disable("x-powered-by")
-  .engine("html", require("ejs").renderFile)  
-  .set("view engine", "html");
+var express = require('express'),
+  app = express(),
+  path = require('path');
 
 app
-  .use(express.logger("tiny"))
-  .use(express.static("public"));
+  .disable('x-powered-by')
+  .engine('html', require('ejs').renderFile)  
+  
+  .set('view engine', 'html')
+  .set('port', process.env.PORT || 3001)
+  .set('views', __dirname + '/views')
+  
+  .use(express.favicon())
+  .use(express.logger('tiny'))
+  .use(express.static(path.join(__dirname, 'public')))
+  .use(express.bodyParser())
+  .use(express.methodOverride())
+  .use(app.router);
 
-app.get("/", function(req, res){
-  res.render("index");
+app.get('/', function(req, res){
+  res.render('index');
 });
 
-app.get("/views/:templateName", function(req, res){
+app.get('/views/:templateName', function(req, res) {
+  console.log(req.params.templateName.yellow)
   res.render(req.params.templateName);
 });
 
 app.listen(3000);
-console.log("start web-server on port 3000".green);
+console.log('start web-server on port 3000'.green);
