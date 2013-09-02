4d8a0b950e5e3e372587096ade6036a76f956eec
vol4ok <admin@vol4ok.net>
2013-09-02 15:07:27 +0300

    Gruntfile: remove watch task from default

--- Gruntfile.js
+++ Gruntfile.js
@@ -88,6 +88,6 @@ module.exports = function(grunt) {
 
   });
 
-  grunt.registerTask('default', ['copy:development', 'less:development', 'concat:development', 'watch']);
+  grunt.registerTask('default', ['copy:development', 'less:development', 'concat:development']);
   grunt.registerTask('build', []);
 };
