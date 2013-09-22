760d1d45e2b3fdac89b2575a68a268544083cc12
putskoo <putskoo@gmail.com>
2013-09-22 21:14:04 +0300

    details controller fix

--- scripts/controllers/details-controller.js
+++ scripts/controllers/details-controller.js
@@ -10,7 +10,7 @@
         $scope._id = currentTalkId;
 
         if (ng.isDefined(currentTalkId)) {
-          $scope.details = $scope.global.data.talks[currentTalkId]; 
+          $scope.details = $scope.global.talks[currentTalkId]; 
           $scope.details.date = new Date($scope.details.date);
           $scope.locationReturn = '/calendar/' + helper.getMonthName($scope.details.date.getMonth(), true) + '/' + $scope.details.date.getFullYear();
         }
