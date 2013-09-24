01ec334ae82d61ea027da3f322c447619ce64f07
putskoo <putskoo@gmail.com>
2013-09-24 13:30:45 +0300

    1. contenteditable ngModel added; 2. 404 page added;

--- scripts/controllers/edit-controller.js
+++ scripts/controllers/edit-controller.js
@@ -2,42 +2,42 @@
   'use strict';
 
   ng.module('tp')
-    .directive('contenteditable', function() {
-      return {
-        restrict: 'A', // only activate on element attribute
-        require: '?ngModel', // get a hold of NgModelController
-        scope: {
-          value: '=ngModel'
-        },
-        link: function($scope, $element, $attrs, ngModel) {
-          if (!ngModel) return; // do nothing if no ng-model
+    .directive('contenteditable', ['$timeout',
+      function($timeout) {
+        return {
+          restrict: 'A', // only activate on element attribute ,
+          require: '?ngModel', // get a hold of NgModelController
+          link: function($scope, $element, $attrs, ngModel) {
+            if (!ngModel) return; // do nothing if no ng-model
 
-          // Specify how UI should be updated
-          ngModel.$render = function() {
-            $element.html(ngModel.$viewValue || '');
-          };
+            // Specify how UI should be updated
+            ngModel.$render = function() {
+              $element.html(ngModel.$viewValue || '');
+            };
 
-          // Listen for change events to enable binding
-          $element.on('blur keyup change', function() {
-            $scope.$apply(read);
-          });
+            // Listen for change events to enable binding
+            $element.on('blur keyup change', function() {
+              $scope.$apply(read);
+            });
 
-          read();
+            $timeout(function() {
+              read();
+            }, 0);
 
-          // Write data to the model
-          function read() {
-            debugger;
-            var html = $element.html();
-            // When we clear the content editable the browser leaves a <br> behind
-            // If strip-br attribute is provided then we strip this out
-            if( $attrs.stripBr && html == '<br>' ) {
-              html = '';
+            // Write data to the model
+            function read() {
+              var html = $element.html();
+              // When we clear the content editable the browser leaves a <br> behind
+              // If strip-br attribute is provided then we strip this out
+              if( $attrs.stripBr && html == '<br>' ) {
+                html = '';
+              }
+              ngModel.$setViewValue(html);
             }
-            ngModel.$setViewValue(html);
           }
         }
       }
-    })
+    ])
     .controller('EditCtrl', ['$scope', '$routeParams', 'data', '$location', 
       function($scope, $routeParams, dataProvider, $location) {
         var currentTalkId = $routeParams.talkId,
