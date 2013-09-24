b4cd0bd10e6aeaa25571d269f23ef513dd6517b0
putskoo <putskoo@gmail.com>
2013-09-24 09:00:56 +0300

    edit page fixes

--- scripts/controllers/edit-controller.js
+++ scripts/controllers/edit-controller.js
@@ -2,6 +2,42 @@
   'use strict';
 
   ng.module('tp')
+    .directive('contenteditable', function() {
+      return {
+        restrict: 'A', // only activate on element attribute
+        require: '?ngModel', // get a hold of NgModelController
+        scope: {
+          value: '=ngModel'
+        },
+        link: function($scope, $element, $attrs, ngModel) {
+          if (!ngModel) return; // do nothing if no ng-model
+
+          // Specify how UI should be updated
+          ngModel.$render = function() {
+            $element.html(ngModel.$viewValue || '');
+          };
+
+          // Listen for change events to enable binding
+          $element.on('blur keyup change', function() {
+            $scope.$apply(read);
+          });
+
+          read();
+
+          // Write data to the model
+          function read() {
+            debugger;
+            var html = $element.html();
+            // When we clear the content editable the browser leaves a <br> behind
+            // If strip-br attribute is provided then we strip this out
+            if( $attrs.stripBr && html == '<br>' ) {
+              html = '';
+            }
+            ngModel.$setViewValue(html);
+          }
+        }
+      }
+    })
     .controller('EditCtrl', ['$scope', '$routeParams', 'data', '$location', 
       function($scope, $routeParams, dataProvider, $location) {
         var currentTalkId = $routeParams.talkId,
@@ -22,8 +58,6 @@
           var title = $scope.global.pageTitle;
           $scope.global.pageTitle = title + LOADING_MSG;
           $scope.loading = true;
-
-          $scope.details.description = document.getElementById('talkContent').innerHTML;
 
           $scope.details.$save(function(data) {
             $scope.loading = false;
